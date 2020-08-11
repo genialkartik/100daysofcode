12b21a27d777b3bdb76501ab7c0159723effb37d
Kartik Tyagi <32240906+genialkartik@users.noreply.github.com>
2020-08-11 03:12:22 -0700

    Create sum_of_n_numbers.cpp

---
+++ sum_of_n_numbers.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  double sum = 0;
+  double n;
+  double inputs;
+  cin>>inputs;
+  for(double i=0; i<inputs; i++){
+    cin>>n;
+    sum += n;
+  }
+  cout<<sum<<endl;
+}
