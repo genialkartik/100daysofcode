b4f8e2a9bccbfdaa3a68acc9c73f6d28524d1b4b
Kartik Tyagi <32240906+genialkartik@users.noreply.github.com>
2020-08-14 15:58:23 +0530

    Update and rename Rotate Matrix Clockwise.cpp to RotateMatrix.cpp

--- CPCIPHER/RotateMatrix.cpp
+++ CPCIPHER/RotateMatrix.cpp
@@ -16,6 +16,17 @@ void rotateMatrix(int mat[N][N])
             mat[j][N-1-i] = temp;
         }
     }
+	// Anti-Clockwise
+	
+	//for (int x = 0; x < N / 2; x++) { 
+		//for (int y = x; y < N - x - 1; y++) {
+		//	int temp = mat[x][y]; 
+			//mat[x][y] = mat[y][N - 1 - x]; 
+			//mat[y][N - 1 - x] = mat[N - 1 - x][N - 1 - y]; 
+			//mat[N - 1 - x][N - 1 - y] = mat[N - 1 - y][x]; 
+			//mat[N - 1 - y][x] = temp; 
+		//} 
+	//} 
 } 
 
 // Function to print the matrix 
