ffb7f4a1e47424be3ca259781ebf5b5eb2e2a2b7
Kartik Tyagi <32240906+genialkartik@users.noreply.github.com>
2020-08-17 09:22:05 +0530

    010

--- CPCIPHER/majorityElement.cpp
+++ CPCIPHER/majorityElement.cpp
@@ -1,14 +1,14 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
-        int count=0,a=0;
+        int count=0,res=0;
         sort(nums.begin(), nums.end());
         if(nums.size() == 1){
             return nums[0];
         }else{
             for(int i=0; i<nums.size()-1; i++){
                 if(nums[i] == nums[i+1]){
-                    a = nums[i];
+                    res = nums[i];
                     count++;
                 }
                 else if(count >= nums.size()/2){
@@ -19,6 +19,6 @@ public:
                 }
             }
         }
-            return a;
+    return res;
     }
 };
