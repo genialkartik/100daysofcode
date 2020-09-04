2a60fd88aa8bde70b5e2f38ae5699702ff2f4d80
genialkartik <kartik9756@gmail.com>
2020-09-04 22:49:07 +0530

    Day 4

--- Day4/PascalsTriangle.cpp
+++ Day4/PascalsTriangle.cpp
@@ -6,9 +6,7 @@ public:
         vector<vector<int>> res;
         for(auto i=0;i<numRows;++i)
         {
-          // initialize every array (2d) with 1
             res.push_back(vector<int>(i+1,1));
-          // make changes in required (middle) elements
             for(auto j=1; j<i; ++j) {
               res[i][j] = res[i-1][j-1] + res[i-1][j];
             }
