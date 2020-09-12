e2ab291c592f0f8e92bfffcfed28fbeee6090209
genialkartik <kartik9756@gmail.com>
2020-09-12 21:30:03 +0530

    int32

---
+++ Day11/ReverseInt.cpp
@@ -0,0 +1,15 @@
+// leetcode.com/problems/reverse-integer
+
+class Solution {
+public:
+    int reverse(int x) {
+        long long res = 0;
+        while(x!=0){
+            res = res*10 + x%10;
+            x /= 10;
+            if(abs(res) > INT_MAX)
+                return 0;
+        }
+        return res;
+    }
+};
