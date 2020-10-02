c79f2246ac06727c2aa09d79febbf3a590eaaf54
genialkartik <kartik9756@gmail.com>
2020-10-02 21:07:31 +0530

    Day 27

---
+++ Day27/HandsOfStraight.cpp
@@ -0,0 +1,25 @@
+// https://leetcode.com/problems/hand-of-straights/
+
+class Solution {
+public:
+    bool isNStraightHand(vector<int>& hand, int W) {
+        multiset<int>s;
+        for(int i=0;i<hand.size();i++){
+            s.insert(hand[i]);
+        }
+        while(s.size()!=0){
+            int a=W-1;
+            int b=*(s.begin());
+            s.erase(s.begin());
+            while(a--){
+                if(s.find(b+1)!=s.end()){
+                    b++;
+                    s.erase(s.find(b));
+                }else{
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+};
